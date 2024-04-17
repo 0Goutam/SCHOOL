971450ac256285e03241da3718c93c64974b0191
0Goutam <167067348+0Goutam@users.noreply.github.com>
2024-04-17 16:02:10 +0530

    Add files via upload

---
+++ school.js
@@ -0,0 +1,201 @@
+function ch()
+{
+    x1=document.getElementById("x1").value ;
+    switch(x1)
+    {
+        case "a":
+        case"A":
+                    document.getElementById("x3").style.backgroundImage = "url('a4.png')";
+                    document.getElementById("x2").innerHTML="Apple"
+                    var audio= new Audio("a.mp3");
+                    audio.play()
+                     break
+        case "b":
+          case"B":
+                    document.getElementById("x2").innerHTML="Ball"
+                    document.getElementById("x3").style.backgroundImage = "url('2.png')";
+                    var audio= new Audio("b.mp3");
+                    audio.play()
+                     break
+        case "c":
+          case"C":
+                    document.getElementById("x3").style.backgroundImage = "url('c.png')";
+                    document.getElementById("x2").innerHTML="Cat"
+                     var audio= new Audio("c.mp3");
+                     audio.play()
+                    break
+        case "d":
+          case"D":
+                    document.getElementById("x3").style.backgroundImage = "url('d.png')";
+                    document.getElementById("x2").innerHTML="Dog"
+                     var audio= new Audio("d.mp3");
+                    audio.play()
+                    break
+        case "e":
+          case"E":
+                   document.getElementById("x3").style.backgroundImage = "url('e.png')";
+                   document.getElementById("x2").innerHTML="Elephant"
+                   var audio= new Audio("e.mp3");
+                   audio.play()
+                   break
+        case "f":
+          case"F":
+                   document.getElementById("x3").style.backgroundImage = "url('f.png')";
+                   document.getElementById("x2").innerHTML="Fish"
+                   var audio= new Audio("f.mp3");
+                   audio.play()
+                    break
+        case "g":
+          case"G":
+                    document.getElementById("x3").style.backgroundImage = "url('g.png')";
+                    document.getElementById("x2").innerHTML="Giraffe"
+                    var audio= new Audio("g.mp3");
+                   audio.play()
+                    break
+            
+        case "h":
+          case"H":
+                    document.getElementById("x3").style.backgroundImage = "url('h.png')";
+                    document.getElementById("x2").innerHTML="Horse"
+                    var audio= new Audio("h.mp3");
+                   audio.play()
+                     break
+        case "i":
+          case"I":
+                    document.getElementById("x3").style.backgroundImage = "url('i.png')";
+                    document.getElementById("x2").innerHTML="Ice cream"
+                    var audio= new Audio("i.mp3");
+                   audio.play()
+                     break
+        case "j":
+          case"J":
+                    document.getElementById("x3").style.backgroundImage = "url('j.png')";
+                    document.getElementById("x2").innerHTML="Jug"
+                    var audio= new Audio("j.mp3");
+                   audio.play()
+                    break
+        case "k":
+          case"K":
+                    document.getElementById("x3").style.backgroundImage = "url('k.png')";
+                    document.getElementById("x2").innerHTML="Kite"
+                    var audio= new Audio("k.mp3");
+                   audio.play()
+                    break
+        case "l":
+          case"L":
+                   document.getElementById("x3").style.backgroundImage = "url('l.png')";
+                   document.getElementById("x2").innerHTML="Lion"
+                   var audio= new Audio("l.mp3");
+                   audio.play()
+                   break
+        case "m":
+          case"M":
+                   document.getElementById("x3").style.backgroundImage = "url('m.png')";
+                   document.getElementById("x2").innerHTML="Monkey"
+                   var audio= new Audio("m.mp3");
+                   audio.play()
+                    break
+        case "n":
+          case"N":
+                    document.getElementById("x3").style.backgroundImage = "url('n.png')";
+                    document.getElementById("x2").innerHTML="Nest"
+                    var audio= new Audio("n.mp3");
+                   audio.play()
+                     break
+        case "o":
+          case"O":
+                    document.getElementById("x3").style.backgroundImage = "url('o.png')";
+                    document.getElementById("x2").innerHTML="Octopus"
+                    var audio= new Audio("o.mp3");
+                   audio.play()
+                     break
+        case "p":
+          case"P":
+                    document.getElementById("x3").style.backgroundImage = "url('p1.png')";
+                    document.getElementById("x2").innerHTML="Panda"
+                    var audio= new Audio("p.mp3");
+                   audio.play()
+                     break
+        case "q":
+          case"Q":
+                    document.getElementById("x3").style.backgroundImage = "url('q.png')";
+                    document.getElementById("x2").innerHTML="Queen"
+                    var audio= new Audio("q.mp3");
+                   audio.play()
+                    break
+        case "r":
+          case"R":
+                    document.getElementById("x3").style.backgroundImage = "url('r.png')";
+                    document.getElementById("x2").innerHTML="Rabbit"
+                    var audio= new Audio("r.mp3");
+                   audio.play()
+                    break
+        case "s":
+          case"S":
+                   document.getElementById("x3").style.backgroundImage = "url('s1.png')";
+                   document.getElementById("x2").innerHTML="Sun"
+                   var audio= new Audio("s.mp3");
+                   audio.play()
+                   break
+        case "t":
+          case"T":
+                   document.getElementById("x3").style.backgroundImage = "url('t.png')";
+                   document.getElementById("x2").innerHTML="Tomato"
+                   var audio= new Audio("t.mp3");
+                   audio.play()
+                    break
+        case "u":
+          case"U":
+                    document.getElementById("x3").style.backgroundImage = "url('u1.png')";
+                    document.getElementById("x2").innerHTML="Umbrella"
+                    var audio= new Audio("u.mp3");
+                   audio.play()
+                     break
+        case "v":
+          case"V":
+                    document.getElementById("x3").style.backgroundImage = "url('v.png')";
+                    document.getElementById("x2").innerHTML="Van"
+                    var audio= new Audio("v.mp3");
+                   audio.play()
+                     break
+        case "w":
+          case"W":
+                    document.getElementById("x3").style.backgroundImage = "url('w.png')";
+                    document.getElementById("x2").innerHTML="Whale"
+                    var audio= new Audio("w.mp3");
+                   audio.play()
+                     break
+        case "x":
+          case"X":
+                    document.getElementById("x3").style.backgroundImage = "url('x.png')";
+                    document.getElementById("x2").innerHTML="X-mas tree"
+                    var audio= new Audio("x.mp3");
+                   audio.play()
+                    break
+        case "y":
+          case"Y":
+                    document.getElementById("x3").style.backgroundImage = "url('y.png')";
+                    document.getElementById("x2").innerHTML="Yak"
+                    var audio= new Audio("y.mp3");
+                   audio.play()
+                    break
+        case "z":
+          case"Z":
+                   document.getElementById("x3").style.backgroundImage = "url('z.png')";
+                   document.getElementById("x2").innerHTML="Zebra"
+                   var audio= new Audio("z.mp3");
+                   audio.play()
+                   break
+        default:
+                    document.getElementById("x2").innerHTML=" ";
+                    document.getElementById("x3").style.backgroundImage = "url('0.png')";
+            }
+}
+function low()
+{
+   document.getElementById("x2").style.textTransform= 'lowercase'
+}
+function up()
+{
+   document.getElementById("x2").style.textTransform= 'uppercase'
+}
